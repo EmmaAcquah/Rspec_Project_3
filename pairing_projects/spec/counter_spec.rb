b7c2ed2c68e23ebdb91b2d86e5d00be2ec5325c4
Asli Champion <30733811+aslichampion@users.noreply.github.com>
2022-10-03 16:27:46 +0100

    counter tests added

--- pairing_projects/spec/counter_spec.rb
+++ pairing_projects/spec/counter_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Counter do
     it "counts up incrementally" do
         counter = Counter.new
         counter.add(5)
+        counter.add(2)
+        counter.add(3)
         result = counter.report
-        expect(result).to eq 'Counted to 5 so far.'
+        expect(result).to eq 'Counted to 9 so far.'
     end
 end
