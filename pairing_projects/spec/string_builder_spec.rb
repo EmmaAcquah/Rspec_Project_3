b665f7e3a43a626155a9b06c1e5a7cbcb3af6ad2
AngeliseK <Angelisekaranja@icloud.com>
2022-10-03 17:04:08 +0100

    b

--- pairing_projects/spec/string_builder_spec.rb
+++ pairing_projects/spec/string_builder_spec.rb
@@ -1,9 +1,16 @@
 require 'string_builder'
 RSpec.describe StringBuilder do 
-    it "counts the characters of a string" do 
-        stringbuilder = StringBuilder.new("")
-        stringbuilder.add("Makers")
-        stringbuilder.ouput
-        expect(result).to eq 'Makers'
+    it "stores and counts the characters of a string" do 
+        stringbuilder = StringBuilder.new
+        stringbuilder.add "Makers"
+        stringbuilder.add " Academy"
+        stringbuilder.add " Sept"
+        stringbuilder.add " Cohort"
+        stringbuilder.output
+        result = stringbuilder.output 
+        expect(result).to eq 'Makers Academy Sept Cohort'
+        length = stringbuilder.size 
+        expect(length).to eq 26 
+
     end 
 end 
