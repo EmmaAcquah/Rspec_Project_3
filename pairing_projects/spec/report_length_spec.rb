3c54d4ece5feb5d65ff6b341a34dc9aee5d2d96d
AngeliseK <Angelisekaranja@icloud.com>
2022-10-03 16:08:13 +0100

    Report length spec

--- pairing_projects/spec/report_length_spec.rb
+++ pairing_projects/spec/report_length_spec.rb
@@ -3,6 +3,6 @@ require 'report_length'
 RSpec.describe "Check the report length" do 
     it "counts the length of the report " do 
         result = report_length('Makers')
-        expect result.to eq "This string was 6 characters long"
+        expect(result).to eq "This string was 6 characters long"
     end 
 end 
